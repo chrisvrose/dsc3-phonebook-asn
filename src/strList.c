18cadc053c81d65bf3269f6d96e2e4e238776ec7
Christopher Rose <christophervrose@gmail.com>
2019-10-04 14:38:57 +0530

    freeNode

--- src/strList.c
+++ src/strList.c
@@ -2,6 +2,7 @@
 
 #define PMNULL ((void*)0)
 
+///obtain node and set vals
 node* getNode(char* data){
     node* newNode;
     if((newNode = malloc(sizeof(node)))){
@@ -10,8 +11,29 @@ node* getNode(char* data){
     return newNode;
 }
 
-//node* free
+///Trash node (getting val is optional)
+void freeNode(node* element,char* outVar){
+    if(element!=PMNULL){
+        if(outVar!=PMNULL){
+            int i;
+            for(i=0;outVar[i]!=0;i++){
+                element->number[i]=outVar[i];
+            }
+            outVar[i]=0;
+        }
+        free(element);
+    }
+}
 
-void removeAll(){
+///Remove all
+void removeAll(node* headNode){
+    if(headNode!=PMNULL){
+        removeAll(headNode->next);
+        free(headNode);
+    }
+}
 
+
+node* insertEnd(node* root){
+    
 }
