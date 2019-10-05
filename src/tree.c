afa0021ff70b1ffaf86926499eb44290b6d17788
Christopher Rose <christophervrose@gmail.com>
2019-10-05 11:33:03 +0530

    Stonks: Implement insertion

--- src/tree.c
+++ src/tree.c
@@ -21,6 +21,10 @@ int getHeight(treeNode* treenode){
     }
 }
 
+
+///Balance:
+///>0: left heavy
+///<0: right heavy
 int getBalance(treeNode* root){
     if(root==NULL){
         return 0;
@@ -94,8 +98,62 @@ treeNode* rightRotate(treeNode* b){
 
 
 
+/*
+    AVL insert:
+    returns:
+        for each: more inserts/
+        on whole: 
+*/
+treeNode* insert(treeNode* root,char* name,char* number){
+    ///Step 1: Perform the bog standard BST insertion
+    if(root==NULL){
+        //We found where to insert, do it
+        return getNewTreeNode(name,number);
+    }
 
-/* treeNode* insert(treeNode* root,char* name,char* number){
-    
-} */
+    int getCmp = strcmp(name,root->name);
+    if(getCmp<0){
+        root->left = insert(root->left,name,number);
+    }else if(getCmp>0){
+        root->right = insert(root->right,name,number);
+    }else{
+        ///Equality, yikes!
+        root->dataHead = insertEnd(root->dataHead,number,NULL);
+    }
+
+    ///Done with insertion, get balance factor
+    int balance = getBalance(root);
+
+    ///Now the tricky part
+    ///2 variables - where the node was pushed wrt root, and the balance
+    /// 4 outcomes
+
+    ///heavy on left side and it got inserted below the left child
+    if(balance>1 && strcmp(name,root->left->name)<0){
+        return rightRotate(root);
+    }
+
+    ///Heavy on the right side, and insertion occured on the right
+    if(balance<-1 && strcmp(name,root->right->name)>0){
+        return leftRotate(root);
+    }
+
+    ///Heavy on the left side, got inserted onto the right of the left child
+    if(balance>1 && strcmp(name,root->left->name)>0){
+        ///Rotate left
+        root->left = leftRotate(root->left);
+        ///Rotate self rightways
+        return rightRotate(root);
+    }
+
+    ///Heavy on right, and got inserted on the left
+    if(balance<-1 && strcmp(name,root->right->name)<0){
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+
+
+    ////for anything else
+    return root;
+}
 
