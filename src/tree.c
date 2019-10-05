3d2ccd012ba76aff1fa4185ec6478f741b6c2921
Christopher Rose <christophervrose@gmail.com>
2019-10-05 10:46:28 +0530

    Add rotations

--- src/tree.c
+++ src/tree.c
@@ -21,11 +21,20 @@ int getHeight(treeNode* treenode){
     }
 }
 
+int getBalance(treeNode* root){
+    if(root==NULL){
+        return 0;
+    }else{
+        return getHeight(root->left)-getHeight(root->right);
+    }
+}
+
+
 treeNode* getNewTreeNode(char* name,char* number){
     treeNode* newNode;
     if((newNode = malloc(sizeof(treeNode)))){
         newNode->left=newNode->right=NULL;
-        newNode->height=0;
+        newNode->height=1;
         strcpy(newNode->name,name);
         newNode->dataHead = insertEnd(NULL,number,NULL);
     }
@@ -50,7 +59,43 @@ treeNode* treeSearch(treeNode* treenode,char* searchElement){
 }
 
 
-treeNode* insert(treeNode* root,char* name,char* number){
 
+
+treeNode* leftRotate(treeNode* b){
+    treeNode* a = b->left;
+    treeNode * treeToChange = a->right;
+
+    ///Make the rotation
+    a->right = b;
+    b->left = treeToChange;
+
+    ///Recalc heights
+    b->height = max(height(b->left),height(b->right))+1;
+    a->height = max(height(a->left),height(a->right))+1;
+
+    ///a becomes the new root
+    return a;
 }
+
+treeNode* rightRotate(treeNode* b){
+    treeNode* a = b->right;
+    treeNode* treeToChange = a->left;
+
+    ///Make rotations
+    a->left = b;
+    b->right = treeToChange;
+
+    ///Recalc heights
+    a->height = max(height(a->left),height(a->right))+1;
+    b->height = max(height(b->left),height(b->right))+1;
+
+    return a;
+}
+
+
+
+
+/* treeNode* insert(treeNode* root,char* name,char* number){
+    
+} */
 
